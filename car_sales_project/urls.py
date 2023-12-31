f361c15ddb8d23911935b47675a7aeadfb189130
mdalaminfaraji <alaminice1617@gmail.com>
2023-12-31 18:58:45 +0600

    create profile page , edit profile , signup, login doen

--- car_sales_project/urls.py
+++ car_sales_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .import views
 urlpatterns = [
     path('', views.home, name="homepage"),
     path('admin/', admin.site.urls),
+    path('user/', include('users.urls'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, update_session_auth_hash, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register(request):
+    if request.method=="POST":
+        register_form=forms.SignUpForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Register successfully done')
+            return redirect('register')
+    else:
+        register_form = forms.SignUpForm()
+    return render(request, 'register.html', {'form':register_form, 'type':'Register'})
+
+
+def user_login(request):
+    if request.method=='POST':
+        form=AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name=form.cleaned_data['username']
+            user_pass=form.cleaned_data['password']
+            user=authenticate(username=user_name, password=user_pass)
+            if user is not None:
+                messages.success(request, 'Login successfully done')
+                login(request, user)
+                return redirect('profile')
+            else:
+                messages.warning(request, 'user do not find please  login')
+                return  redirect('register')
+          
+    else:
+        form=AuthenticationForm()
+        return render(request, 'register.html', {'form':form, 'type':'login'})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html', {"data":request.user})
+
+@login_required
+def edit_profile(request):
+    if request.method=="POST":
+        profile_form=forms.ChangeUserData(request.POST, instance=request.user)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully done')
+            return redirect('profile')
+    else:
+        profile_form = forms.ChangeUserData( instance=request.user)
+    return render(request, 'update_profile.html', {'form':profile_form, 'type':'Profile', "user":request.user})
+
